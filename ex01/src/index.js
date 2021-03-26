c22326729f2533315c965f81f8b471dc008b1414
adiheric <hadiheric@gmail.com>
2021-03-26 10:37:46 +0100

    day18

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+let myArr = [];
+function* multiplication(x) {
+  for (let i = 0; i < x; i++) {
+    x *= 2;
+    myArr.push(x);
+    yield x;
+  }
+}
+
+const num = multiplication(3);
+
+for (let j = 0; j < 3; j++) {
+  console.log(num.next().value);
+}
+module.exports = multiplication;
---
+++ ex05/src/index.js
@@ -0,0 +1,23 @@
+let shoppingList = new Map([
+	["Banana", 3],
+	["Pineapple", 5],
+	["Pear", 2],
+	["Carrot", 10],
+	
+]);
+let arrKey = [];
+let arrValue = [];
+for (let col of shoppingList.entries()) {
+	arrKey.push(col[0]);
+	arrValue.push(col[1]);
+}
+arrKey.forEach((key) => {
+	console.log(`groceries: ${key}`);
+});
+arrValue.forEach((value) => {
+	console.log(`amount: ${value}`);
+});
+for (let col of shoppingList.entries()) {
+	console.log(col);
+}
+module.exports = { shoppingList, arrKey, arrValue };
